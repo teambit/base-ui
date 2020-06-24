928de845f01725ebd963c7bec040fd496f95d510
Uri Kutner <urikutner@gmail.com>
2020-06-24 14:34:54 +0300

    fix bedrock exports

--- src/surfaces/bedrock/bedrock.tsx
+++ src/surfaces/bedrock/bedrock.tsx
@@ -4,7 +4,7 @@ import { bedrockClass } from './bedrock.classes';
 
 type BedrockProps = React.HTMLAttributes<HTMLDivElement>;
 
-export default function Bedrock(props: BedrockProps){
+export function Bedrock(props: BedrockProps){
 	return (
 		<div {...props} className={classNames(bedrockClass, props.className)}/>
 	)
